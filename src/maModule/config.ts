9e4bbb8ca5bf6e78c471149ff5412e6b7980f2b3
Anthony.xu <xianjin-anthony.xu@se.com>
2019-12-25 15:24:17 +0800

    增加机器顾问校验一级导航菜单的业务逻辑(目前仅2个菜单)

---
+++ src/maModule/config.ts
@@ -0,0 +1,17 @@
+//该页面用于配置每个一级导航菜单的链接及相关验证内容
+let config = {
+  "menuItems":[
+    {
+      "name":"myAsset",
+      "url":"https://dttest.mm.energymost.com/zh-cn/5662d/asset/info/5662d/basic",
+      "validClass":"div.pop-viewable-title"
+    },
+    {
+      "name":"alarmList",
+      "url":"https://dttest.mm.energymost.com/zh-cn/5662d/alarm/20___false_false______AlarmTime;true/1",
+      "validClass":"div.alarm-tab-item"
+    }
+  ]
+}
+
+export default config
--- src/maModule/index.ts
+++ src/maModule/index.ts
@@ -2,9 +2,36 @@ import * as config from "../prodConfig"
 import * as puppeteer from "puppeteer"
 import { getLogger } from "log4js"
 import { screenshot, isLocatorReady } from '../util';
+import maConfig from "./config"
 
 const logger = getLogger("maModule")
+const timeoutOption = { timeout: 15000 }
+
+
+/**
+ * @description 单独校验每一个菜单的配置项
+ * @param {*} item 菜单配置
+ * @param {puppeteer.Page} page
+ */
+async function validMenu(item: any, page: puppeteer.Page) {
+  await page.goto(item.url);
+  let findElement = await page.waitForSelector(item.validClass, timeoutOption);
+  if (findElement) {
+    logger.info(`menu ${item.name} element[${item.validClass}] is shown`);
+  }
+  else {
+    logger.error(`menu ${item.name} element[${item.validClass}] was not found`);
+  }
+  var imgName = `Hiphop-${item.name}`;
+  await screenshot(page, imgName);
+}
 
 export async function main(config: config.Config, page: puppeteer.Page) {
   //to do here
+  if (maConfig != null && maConfig.menuItems != null) {
+    for (let index = 0; index < maConfig.menuItems.length; index++) {
+      const item = maConfig.menuItems[index];
+      await validMenu(item, page);      
+    }
+  }
 }
