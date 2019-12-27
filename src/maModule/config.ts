a00155780dd0749ca4bc104feac7c1ab017185c0
Anthony.xu <xianjin-anthony.xu@se.com>
2019-12-27 08:30:32 +0800

    Feature:基本完成MA的一级菜单的监控功能

--- src/maModule/config.ts
+++ src/maModule/config.ts
@@ -1,15 +1,66 @@
 //该页面用于配置每个一级导航菜单的链接及相关验证内容
 let config = {
+  "menuSelector":"div.pop-menu > a",
   "menuItems":[
     {
       "name":"myAsset",
-      "url":"https://dttest.mm.energymost.com/zh-cn/5662d/asset/info/5662d/basic",
-      "validClass":"div.pop-viewable-title"
+      "isSubMenu": false,
+      "url":"/zh-cn/#cCode#/asset/",
+      "validClass":["div.pop-viewable-title"]
     },
     {
       "name":"alarmList",
-      "url":"https://dttest.mm.energymost.com/zh-cn/5662d/alarm/20___false_false______AlarmTime;true/1",
-      "validClass":"div.alarm-tab-item"
+      "isSubMenu": false,
+      "url":"/zh-cn/#cCode#/alarm/",
+      "validClass":["div.alarm-tab-item"]
+    },
+    {
+      "name":"maintenancePlan",
+      "isSubMenu": false,
+      "url":"/zh-cn/#cCode#/maintenance/",
+      "validClass":["a.maintenance-header-tab"]
+    },
+    {
+      "name":"ticket",
+      "isSubMenu": false,
+      "url":"/zh-cn/#cCode#/ticket/",
+      "validClass":["a.ticket-tab-link"]
+    },
+    {
+      "name":"aftersales",
+      "isSubMenu": false,
+      "url":"/zh-cn/#cCode#/aftersales/",
+      "validClass":["div.ant-tabs-extra-content"]
+    },
+    {
+      "name":"doc",
+      "isSubMenu": false,
+      "url":"/zh-cn/#cCode#/doc/",
+      "validClass":"i.anticon-file-add"
+    },
+    {
+      "name":"comXGatewayManagement",
+      "isSubMenu": true,
+      "url":"/zh-cn/#cCode#/gateway/comx/",
+      "validClass":["div.new-gateway-tools"],
+    },
+    {
+      "name":"IOTGatewayManagement",
+      "isSubMenu": true,
+      "url":"https://#EMOPHost#/doorbell?path=/gateway/#cId#&sysId=2&spDomain=#spDomain#&userId=#uId#",
+      "validClass":["div.gatewayBar"],
+    },
+    {
+      "name":"dataeye",
+      "isSubMenu": false,
+      "url":"/zh-cn/dataeye?customerId=#cId#&userId=#uId#",
+      "validClass":["div.addBoard","div.add-board"],    //因dataEye中有数据和无数据的样式表不一样
+    },
+    {
+      "name":"tagManagement",
+      "isSubMenu": false,
+      "url":"https://#EMOPHost#/doorbell?path=/tag/#cId#&sysId=2&spDomain=#spDomain#&userId=#uId#",
+      "validClass":["div.dropdown-group"],
     }
   ]
 }
--- src/maModule/index.ts
+++ src/maModule/index.ts
@@ -2,36 +2,95 @@ import * as config from "../prodConfig"
 import * as puppeteer from "puppeteer"
 import { getLogger } from "log4js"
 import { screenshot, isLocatorReady } from '../util';
+import {pushDuration, pushStatus} from '../pushGateway';
 import maConfig from "./config"
+import e = require("express");
 
-const logger = getLogger("maModule")
-const timeoutOption = { timeout: 15000 }
-
+const logger = getLogger("maModule");
+const timeoutOption = { timeout: 15000 };
+const PALADIN_EMOP_MAINURI = process.env["PALADIN_EMOP_MAIN_URI"];
 
 /**
  * @description 单独校验每一个菜单的配置项
  * @param {*} item 菜单配置
  * @param {puppeteer.Page} page
  */
-async function validMenu(item: any, page: puppeteer.Page) {
-  await page.goto(item.url);
-  let findElement = await page.waitForSelector(item.validClass, timeoutOption);
-  if (findElement) {
-    logger.info(`menu ${item.name} element[${item.validClass}] is shown`);
+async function validMenu(config: config.Config, url: string, item: any, page: puppeteer.Page) {
+  let startTime: any = new Date()
+  let endTime: any = new Date()
+  let duration: number = 0
+  let rst = false;
+  await page.goto(url, timeoutOption);
+  for (let i = 0; i < item.validClass.length; i++) {
+    let vc = item.validClass[i];
+    let findElement;
+    try {
+      findElement = await page.waitForSelector(vc, timeoutOption);
+      rst = await isLocatorReady(findElement, page);
+      if (rst) {
+        logger.debug(`menu ${item.name} element[${vc}] is shown`);
+        break;
+      }
+      else  {
+        logger.warn(`menu ${item.name} element[${vc}] was not shown`);
+      }
+    }
+    catch {
+      logger.warn(`menu ${item.name} element[${vc}] was not found or not shown`);
+    }
   }
-  else {
-    logger.error(`menu ${item.name} element[${item.validClass}] was not found`);
-  }
-  var imgName = `Hiphop-${item.name}`;
+  if (rst) {
+    logger.info(`menu ${item.name} element[${item.validClass}] check success`);
+  } else {
+    logger.error(`menu ${item.name} element[${item.validClass}] check failed`);
+  }  
+  var imgName = `${item.name}-${rst ? "success" :"error"}`;
   await screenshot(page, imgName);
+  endTime = new Date();
+  duration = (endTime - startTime) / 1000;
+  pushDuration(config.prodAlias, duration,item.name);
+  return rst;
+}
+
+function encodeId(id: any){
+  var n = Number(id);
+  return n.toString(16);
 }
 
 export async function main(config: config.Config, page: puppeteer.Page) {
   //to do here
   if (maConfig != null && maConfig.menuItems != null) {
+    let emopHost = PALADIN_EMOP_MAINURI.split('/')[2];
+    let spDomain = config.mainUri.replace("https://","").split('.')[0];
+    logger.info(`get current spDomian from mainUrl ${spDomain}`);
+    let cookieList = await page.cookies(config.mainUri);
+    let cId = cookieList.find(cookie => cookie.name === 'CUSTOMERID').value;
+    let cCode = encodeId(cId);
+    logger.info(`get current customer from cookie ${cId} => ${cCode}`);
+    let uId = cookieList.find(cookie => cookie.name === 'UserId').value;
+    logger.info(`get current userId from cookie ${uId}`);
+    // let linkList:string[] = await page.$$eval(maConfig.menuSelector, x => {
+    //   return [...x].map(item => item.getAttribute("href"))
+    // });
+    // logger.info(linkList);
+    let count = 0;
     for (let index = 0; index < maConfig.menuItems.length; index++) {
       const item = maConfig.menuItems[index];
-      await validMenu(item, page);      
+      let url: string = "";
+      url = item.url
+        .replace("#cCode#", cCode)
+        .replace("#cId#", cId)
+        .replace("#uId#", uId)
+        .replace("#spDomain#",spDomain)
+        .replace("#EMOPHost#",emopHost);
+      if (url && url != undefined && url != null && url.indexOf("/zh-cn") == 0) url = config.mainUri + url;            
+      logger.info(`begin to check the menu ${item.name} and check url is ${url}`);
+      if (url) {
+        let rst = await validMenu(config, url, item, page);
+        if (rst) count++; 
+      }
     }
+    //最外层已经有推送单次产品状态了
+    //pushStatus(config.prodAlias, count < maConfig.menuItems.length ? 1 : 0);
   }
 }
